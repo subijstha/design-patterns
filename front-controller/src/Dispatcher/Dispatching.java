492525d5d0a5a7132a6b36ff31eb55620fc95613
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-10-16 15:30:06 -0600

    Dispatcher modified

--- front-controller/src/Dispatcher/Dispatching.java
+++ front-controller/src/Dispatcher/Dispatching.java
@@ -2,8 +2,12 @@ package Dispatcher;
 
 import View.StudentView;
 import View.TeacherView;
+import View.iView;
+
+import java.util.HashMap;
 
 public class Dispatching {
+    /*
     private StudentView studentView;
     private TeacherView teacherView;
 
@@ -20,6 +24,27 @@ public class Dispatching {
             teacherView.display();
         }
     }
+    */
+    private HashMap<String, iView> requestMap = new HashMap<>();
+    private HashMap<String, iView> errorMap = new HashMap<>();
 
+    public void addRequestMapping(String viewName, iView view){
+        requestMap.put(viewName, view);
+    }
+
+    public void addErrorMapping(String viewName, iView view){
+        errorMap.put(viewName, view);
+    }
+
+    public void dispatchRequest(String request) {
+        iView requestView;
+        if ((requestView = requestMap.get(request)) != null) {
+            requestView.display();
+        } else if ((requestView = errorMap.get("NOPAGE")) != null) {
+            requestView.display();
+        } else if ((requestView = errorMap.get("DEFAULT")) != null) {
+            requestView.display();
+        }
+    }
 
 }
