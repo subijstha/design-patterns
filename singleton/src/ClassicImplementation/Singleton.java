b7764ee3a097a231e60428ec05447b85776428ad
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-10-16 17:02:22 -0600

    Singleton classic method

--- singleton/src/ClassicImplementation/Singleton.java
+++ singleton/src/ClassicImplementation/Singleton.java
@@ -1,8 +1,9 @@
 package ClassicImplementation;
-
+//not thread-safe
 public class Singleton {
     //object of singleton class
     private static Singleton instance;
+
 
     //constructor private so that this class cannot be instantiated
     private Singleton(){
@@ -15,8 +16,9 @@ public class Singleton {
         }
         return instance;
     }
-
+//optional method
     public void showMessage(){
         System.out.println("Hello Singleton");
+
     }
 }
