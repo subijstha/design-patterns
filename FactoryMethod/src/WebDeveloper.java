f34ddcc1454aa62afa6d7e130284fd819c394a71
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-10-17 19:17:51 -0600

    FactoryPattern Implementation

---
+++ FactoryMethod/src/WebDeveloper.java
@@ -0,0 +1,7 @@
+public class WebDeveloper implements Employee{
+    @Override
+    public int getSalary() {
+        System.out.println("Getting web developer salary : ");
+        return 49000;
+    }
+}
